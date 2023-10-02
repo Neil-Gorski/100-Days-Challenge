6ec75fdaf387c45f9e327eb6374153cddb5e1067
Neil Gorski <neil.gorski@hotmail.ch>
2023-10-02 14:10:37 +0200

    Flask Final Project - Higher or Lower URLs

--- day-55/hallo.py
+++ day-55/hallo.py
@@ -2,6 +2,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def make_bold(funtion):
+    def wrapper_funtion():
+        text = funtion()
+        return f"<b>{text}</b>"
+    return wrapper_funtion
+
+def make_emphasis(funtion):
+    def wrapper_funtion():
+        text = funtion()
+        return f"<em>{text}</em>"
+    return wrapper_funtion
+
+
+def make_underline(funtion):
+    def wrapper_funtion():
+        text = funtion()
+        return f"<u>{text}</u>"
+    return wrapper_funtion
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, welt! was ist los</p>"
+
+
+@app.route("/bye")
+@make_bold
+@make_underline
+@make_emphasis
+def bye():
+    return "Bye!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ higher-lower/server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from random import randint
+
+
+app = Flask(__name__)
+
+secret_num = randint(0, 9)
+
+@app.route("/")
+def home():
+    return (f"<h1>Guess a number between 0 and 9</h1>"
+            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>")
+
+
+@app.route("/<int:number>")
+def guess_number(number):
+    if number < secret_num:
+        return (f"<h1 style='color:purple;'>The number is too low</h1>"
+                "<img src='https://media.giphy.com/media/oJWx7MtpR2qdi/giphy.gif'>")
+    elif number > secret_num:
+        return (f"<h1 style='color:red;'>The number is too high</h1>"
+                "<img src='https://media.giphy.com/media/3o8doVAxrMjXbIHaU0/giphy.gif'>")
+    else:
+        return (f"<h1 style='color:black;'>You find me!</h1>"
+                "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
