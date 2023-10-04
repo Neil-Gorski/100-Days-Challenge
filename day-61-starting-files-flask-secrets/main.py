e54a93f7f2adac34cc49c02a2ad375e70a53ed2c
Neil Gorski <neil.gorski@hotmail.ch>
2023-10-04 19:00:35 +0200

    Bootstrap-Flask Supports WTForms

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 
 class LoginForm(FlaskForm):
@@ -15,6 +16,7 @@ class LoginForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
+bootstrap = Bootstrap5(app)
 
 
 @app.route("/")
