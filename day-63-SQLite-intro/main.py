fe2a0f5f0d362cfe73cf4fcdb15249563c4eed7c
Neil Gorski <neil.gorski@hotmail.ch>
2023-10-08 10:43:50 +0200

    Add CRUD to Test

--- day-63-SQLite-intro/main.py
+++ day-63-SQLite-intro/main.py
@@ -9,9 +9,9 @@ db_uri = f"sqlite:///{db_path}"
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 db = SQLAlchemy(app)
 
-class  Books(db.Model):
+class  Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique=True, nullable=False)
+    title = db.Column(db.String, unique=False, nullable=False)
     author = db.Column(db.String, nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
@@ -22,13 +22,34 @@ with app.app_context():
     db.create_all()
 
 
+# Create new book
+# with app.app_context():
+#     new_book = Book(title="Herry Potter", author="J.K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
 
+# Read all the books
 with app.app_context():
-    new_book = Books(id=1, title="Herry Potter", author="J.K. Rowling", rating=9.3)
-    db.session.add(new_book)
+    result = db.session.execute(db.select(Book).order_by("id"))
+    all_books = result.scalars()
+
+# Read A Particular Record By Query
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Herry Potter")).scalar()
+    print(book.id)
+
+# Update A Particular Record PRIMARY KEY
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == 2)).scalar()
+    book_to_update.title = "Harry Potter and the Chamber of Secrets"
     db.session.commit()
 
-
+# Delete A Particular Record By PRIMARY KEY
+book_id = 4
+with app.app_context():
+    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
 
 
 
