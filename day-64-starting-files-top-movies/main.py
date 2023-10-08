290df02aee5b5c2e86b17afa4fdb4aa8b166f5f9
Neil Gorski <neil.gorski@hotmail.ch>
2023-10-08 22:50:34 +0200

    ein mal push bitte

--- day-64-starting-files-top-movies/main.py
+++ day-64-starting-files-top-movies/main.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import os
-=======
->>>>>>> d0b409003b43fecb8f8d364a683a0d7a739dbb69
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -10,10 +7,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-<<<<<<< HEAD
-
-=======
->>>>>>> d0b409003b43fecb8f8d364a683a0d7a739dbb69
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -28,51 +21,13 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-<<<<<<< HEAD
-db_path = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'movies.db')}"
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config["SQLALCHEMY_DATABASE_URI"] = db_path
-Bootstrap5(app)
-db = SQLAlchemy(app)
-
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False, unique=True)
-    year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.String, nullable=False)
-    img_url = db.Column(db.String, nullable=False)
-
-    def __repr__(self) -> str:
-        return f"<Movie: {self.title}>"
-
-    @property
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__tabel__.columns}
-
-
-with app.app_context():
-    db.create_all()
-=======
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
->>>>>>> d0b409003b43fecb8f8d364a683a0d7a739dbb69
 
 
 @app.route("/")
 def home():
-<<<<<<< HEAD
-    all_movies = None
-    with app.app_context():
-        result = db.session.execute(db.select(Movie).order_by(Movie.title))
-        all_movies = result.scalars().all()
-    return render_template("index.html", movies=all_movies)
-=======
     return render_template("index.html")
->>>>>>> d0b409003b43fecb8f8d364a683a0d7a739dbb69
 
 
 if __name__ == '__main__':
