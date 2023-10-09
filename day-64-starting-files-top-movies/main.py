5949a906a89ff7c4c832ad4e7d251584a3862c13
Neil Gorski <neil.gorski@hotmail.ch>
2023-10-09 20:56:24 +0200

    push test

--- day-64-starting-files-top-movies/main.py
+++ day-64-starting-files-top-movies/main.py
@@ -7,6 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
+
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -21,13 +22,42 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
+db_path = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'movies.db')}"
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SQLALCHEMY_DATABASE_URI"] = db_path
 Bootstrap5(app)
+db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Movie: {self.title}>"
+
+    @property
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__tabel__.columns}
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = None
+    with app.app_context():
+        result = db.session.execute(db.select(Movie).order_by(Movie.title))
+        all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
